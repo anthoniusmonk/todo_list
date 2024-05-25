6ec4a92b7bc3aa937f015a8e738ec6afa180363b
Anthony Gallardo <anthoniusmonk@gmail.com>
2024-05-25 15:21:48 -0600

    second commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $.ajax({
+    type: 'GET',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      response.tasks.forEach(function (task) {
+        console.log(task.content);
+      })
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+});
