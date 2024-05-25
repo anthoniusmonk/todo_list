5a084950e680e7069db62141662f513d0f67eaac
Anthony Gallardo <anthoniusmonk@gmail.com>
2024-05-25 16:24:00 -0600

    third commit

--- index.js
+++ index.js
@@ -5,11 +5,36 @@ $(document).ready(function(){
     dataType: 'json',
     success: function (response, textStatus) {
       response.tasks.forEach(function (task) {
-        console.log(task.content);
+        $('#todo-list').append('<p>' + task.content + '</p>');
       })
     },
     error: function (request, textStatus, errorMessage) {
       console.log(errorMessage);
     }
+  });
+
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1252',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        console.log(response);
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });  
+  }
+
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
   });
 });
