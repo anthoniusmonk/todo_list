7bca9028e9e418d8be878e814b09f7955f5f056c
Anthony Gallardo <anthoniusmonk@gmail.com>
2024-05-26 16:11:56 -0600

    final draft

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(".demo.index").ready(function(){
       success: function (response, textStatus) {
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
-          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id"' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
+          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '"><i class="fas fa-trash"></i></button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
         });
       },
       error: function (request, textStatus, errorMessage) {
@@ -73,13 +73,27 @@ $(".demo.index").ready(function(){
     });
   }
 
+  var markTaskActive = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=1252',
+      dataType: 'json',
+      success : function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
   $(document).on('change', '.mark-complete', function () {
     if (this.checked) {
       markTaskComplete($(this).data('id'));
+    } else {
+      markTaskActive($(this).data('id'));
     }
   });
 
   getAndDisplayAllTasks();
-  
-
 });
